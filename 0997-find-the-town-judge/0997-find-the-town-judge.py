57fd44fd0363c25ecbb2aabdb3b83ac0caae0ba6
Animesh kumar <tiwari.animesh1sep2006@gmail.com>
2026-08-19 10:12:36 +0530

    Time: 14 ms (71.97%), Space: 16.2 MB (7.18%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        in_degree=[0]*(n+1)
+        out_degree=[0]*(n+1)
+        for a ,b in trust:
+            in_degree[b] +=1
+            out_degree[a] +=1
+        for i in range (1,n+1):
+            if in_degree[i] == n - 1 and out_degree[i] == 0:
+                return i
+        return -1
+
+
+       
+      
+        
